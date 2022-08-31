26c1f445abd2160e0ae3582376e693d4d57b089f
Andrea Rosentel <andrearosentel@gmail.com>
2022-08-31 12:54:04 -0700

    commit tree 63ce80bd7107520bc0051b8d832aa8fb08872310

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,4 +1,5 @@
 require_relative './node'
+require 'pry'
 
 class LinkedList
   attr_accessor :head
@@ -8,7 +9,28 @@ class LinkedList
   end
 
   def nth_from_end(n)
-    # your code here
+    elements = []
+    current = @head
+    # binding.pry
+    if current != nil
+      while current.next_node != nil
+        elements << current.value
+        current = current.next_node
+      end
+    else
+      return nil
+    end
+    elements << current.value
+    # binding.pry
+    if elements.empty?
+      return nil
+    else
+      if (n <= elements.size && n > 0)
+        elements[elements.size - n]
+      else
+        return nil
+      end
+    end
   end
 
 end
